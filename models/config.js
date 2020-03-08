516b3ae096563f10f38359cd18b0dea89db6274b
LouisSoleil <43573845+LouisSoleil@users.noreply.github.com>
2020-03-08 16:27:06 +0100

    try fix heroku fail

--- models/config.js
+++ models/config.js
@@ -1,13 +1,17 @@
 let mysql      = require('mysql');
-let connection = mysql.createConnection({
+let connection = mysql.createPool({
   host     : 'eu-cdbr-west-02.cleardb.net',
   user     : 'be83953ced3eb9',
   password : '5a2985c4',
   database : 'heroku_dca756dd59c7a3f'
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
+
+connection.getConnection((err, connect) => {
+    if(err)
+        console.error("Something went wrong connecting to the database ...");
+    if(connect)
+        connect.release();
 });
 
 module.exports = connection;
